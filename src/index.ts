8e8bfaa13107d1c52050c998f918b78900294a1f
Thibault MJS <thibault.minneboo.js@gmail.com>
2022-08-08 01:31:42 +0200

    add .env config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import TehaClient from "./structure/TehaClient";
 
-const bot = new TehaClient("OTk0NTg0NjI1NzY0NjQyODI3.GUn48B.YA-Nn7TZ6Wl3i224sz6yRCWzDyYG1Ik_rkR8zc", '?');
+const bot = new TehaClient(process.env.TOKEN as string, '?');
 bot.initialize();
